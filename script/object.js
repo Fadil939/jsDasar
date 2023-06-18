242e3bc76dcd0bf3471041ceee4a611baafdce61
Fadil939 <fadhlurrahman939@gmail.com>
2023-06-18 17:08:13 +0700

    memahami function decration dan custructor

--- script/object.js
+++ script/object.js
@@ -7,3 +7,24 @@ let dataDiri = {
     kota: "babelan",
   },
 };
+
+//!function declaration
+function buatBiodata(nama, umur, Jkelamin, jurusan) {
+  let data = {};
+  data.nama = nama;
+  data.umur = umur;
+  data.Jkelamin = Jkelamin;
+  data.jurusan = jurusan;
+  return data;
+}
+let dataDiri2 = buatBiodata("fadil", 18, "laki-laki", "Teknik Mesin");
+
+//!constructor
+//function yang khusus membuat object
+function Biodata(nama, umur, jurusan, email) {
+  this.nama = nama;
+  this.umur = umur;
+  this.jurusan = jurusan;
+  this.email = email;
+}
+let dataDiri3 = new Biodata("rahman", 19, "Teknik Komputer Jaringan", "rahman929@gmail.com");
